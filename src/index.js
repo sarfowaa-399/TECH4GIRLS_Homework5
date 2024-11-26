2abeae38a506e61cb5440d492b62c264caffdfb9
Your Name <amasarfowaap@gmail.com>
2024-11-26 15:43:36 +0000

    made changes in my index.html and added files in my index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let friend = prompt("Enter your name");
+let age = prompt("Enter your age");
+let email = prompt("Enter your email address");
+
+
+alert(`Hi your name is ${friend}, you are ${age} years old and we will keep in touch via your email: ${email} 👋 👋 `);
